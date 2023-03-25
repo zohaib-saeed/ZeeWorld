66d9363272a98f4b89779ce3e76707c9b5703818
zohaibzyebi <zyebi553@gmail.com>
2023-03-25 06:44:17 +0500

    done: drawer

---
+++ src/components/Shared/Layout/NavDrawer.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Link from "next/link";
+import Drawer from "react-modern-drawer";
+import "react-modern-drawer/dist/index.css";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+
+const NavDrawer = ({ isOpen, toggleDrawer }) => {
+  // Nav Links Data
+  const navItems = [
+    { title: "Evenementen", url: "/" },
+    { title: "Ontdekken", url: "/" },
+    { title: "Nieuws", url: "/" },
+  ];
+  return (
+    <Drawer
+      open={isOpen}
+      onClose={toggleDrawer}
+      direction="left"
+      duration={500}
+      className="!w-[98%] md:!w-[50%]"
+    >
+      <div className="w-full h-screen overflow-y-scroll flex flex-col items-center justify-start gap-24 p-5  md:p-8">
+        {/* Close Button  */}
+        <div className="w-full flex items-center justify-end ">
+          <XMarkIcon
+            className="w-[32px] h-[32px] text-black-ebony"
+            onClick={toggleDrawer}
+          />
+        </div>
+        {/* Nav items */}
+        <div className="w-full flex flex-col items-center justify-start gap-20">
+          <div className="flex flex-col items-center justify-start gap-5 ">
+            {navItems.map((item, index) => (
+              <Link key={index} href={item.url}>
+                <div className="font-bold text-black-ebony text-base cursor-pointer relative nav-item ">
+                  {item.title}
+                </div>
+              </Link>
+            ))}
+          </div>
+          {/* Button => Download app  */}
+          <Link href={`/`}>
+            <button
+              className={`transition-all flex items-center justify-center rounded-full px-8 py-3 text-sm font-semibold  text-white cursor-pointer bg-azure hover:text-white`}
+            >
+              Download App
+            </button>
+          </Link>
+        </div>
+      </div>
+    </Drawer>
+  );
+};
+
+export default NavDrawer;
--- src/components/Shared/Layout/Navbar.js
+++ src/components/Shared/Layout/Navbar.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { Bars3Icon } from "@heroicons/react/24/outline";
 
 import Container from "../Container/Container";
 import Button from "../Button/Button";
+import NavDrawer from "./NavDrawer";
 
 const Navbar = ({ logoSize }) => {
+  const [openDrawer, setOpenDrawer] = useState(false);
+
+  function toggleDrawer() {
+    setOpenDrawer(!openDrawer);
+  }
+
   // Nav Links Data
   const navItems = [
     { title: "Evenementen", url: "/" },
@@ -15,43 +22,55 @@ const Navbar = ({ logoSize }) => {
   ];
 
   return (
-    <Container maxWidth="max-w-[1515px]">
-      <div className="w-full flex items-center justify-between gap-3 ">
-        {/* Logo  */}
-        <Link href="/">
-          <div
-            className={`${logoSize} relative flex items-center justify-center -ml-[6px]`}
-          >
-            <Image
-              src={`/images/logo.svg`}
-              alt=""
-              fill
-              className="object-contain"
-            />
+    <React.Fragment>
+      <Container maxWidth="max-w-[1515px]">
+        <div className="w-full flex items-center justify-between gap-3 ">
+          {/* Logo  */}
+          <Link href="/">
+            <div
+              className={`${logoSize} relative flex items-center justify-center -ml-[6px]`}
+            >
+              <Image
+                src={`/images/logo.svg`}
+                alt=""
+                fill
+                className="object-contain"
+              />
+            </div>
+          </Link>
+          {/* Nav Items  */}
+          <div className="hidden lg:flex items-center justify-center gap-6 ">
+            {navItems.map((item, index) => (
+              <Link key={index} href={item.url}>
+                <div className="font-bold text-white text-base cursor-pointer relative nav-item ">
+                  {item.title}
+                </div>
+              </Link>
+            ))}
           </div>
-        </Link>
-        {/* Nav Items  */}
-        <div className="hidden lg:flex items-center justify-center gap-6 ">
-          {navItems.map((item, index) => (
-            <Link key={index} href={item.url}>
-              <div className="font-bold text-white text-base cursor-pointer ">
-                {item.title}
-              </div>
-            </Link>
-          ))}
+          {/* Button => Download app  */}
+          <Link href={`/`}>
+            <button
+              className={`transition-all hidden lg:flex items-center justify-center rounded-full px-8 py-3 text-sm font-semibold  text-white cursor-pointer bg-azure hover:text-white`}
+            >
+              Download App
+            </button>
+          </Link>
+          {/* Menu Button  */}
+          <Bars3Icon
+            onClick={toggleDrawer}
+            className="w-[30px] sm:w-[36px] h-[30px] sm:h-[36px] font-bold text-white cursor-pointer inline-block lg:hidden"
+          />
         </div>
-        {/* Button => Download app  */}
-        <Link href={`/`}>
-          <button
-            className={`transition-all hidden lg:flex items-center justify-center rounded-full px-8 py-3 text-sm font-semibold  text-white cursor-pointer bg-azure hover:text-white`}
-          >
-            Download App
-          </button>
-        </Link>
-        {/* Menu Button  */}
-        <Bars3Icon className="w-[30px] sm:w-[36px] h-[30px] sm:h-[36px] font-bold text-white cursor-pointer inline-block lg:hidden" />
-      </div>
-    </Container>
+      </Container>
+      {openDrawer && (
+        <NavDrawer
+          isOpen={openDrawer}
+          setIsOpen={setOpenDrawer}
+          toggleDrawer={toggleDrawer}
+        />
+      )}
+    </React.Fragment>
   );
 };
 
